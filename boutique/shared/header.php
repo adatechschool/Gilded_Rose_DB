7142cde987308b66b777feefc2636906ac072b61
FlorianBouteille <34886355+FlorianBouteille@users.noreply.github.com>
2020-02-11 15:45:02 +0100

    début de header

---
+++ boutique/shared/header.php
@@ -0,0 +1,8 @@
+<nav class="conteneur">
+        <div class="dropdown">Menu déroulant</div>
+        <form>
+            <input type="search" placeholder="Search" aria-label="Search">
+            <button type="submit">Search</button>
+        </form>
+        <div class="onverra">inscription/connexion/panier</div>
+</nav>
