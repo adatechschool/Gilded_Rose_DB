3f23db9b3db9fdd4104810ca7ebb0ca80269911a
lydiasan <lyd.sanaa@gmail.com>
2020-02-11 12:19:44 +0100

    ajout des fichiers test, text, et principal en .py

---
+++ gilded_rose.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+
+class GildedRose(object):
+
+    def __init__(self, items):
+        self.items = items
+
+    def update_quality(self):
+
+        for item in self.items:
+            item.sell_in -=1
+           
+            if item.category == "collectors":
+                item.quality = item.quality
+
+            if item.category == "others": 
+                item.quality -= 1
+                if "Conjured" in item.name:
+                    item.quality -= 1
+                if item.sell_in < 0:
+                    item.quality = 0
+
+            if item.category == "cheeses" and item.quality < 50:
+                item.quality += 2
+
+            if item.category == "events" and item.quality < 50:
+                if item.sell_in < 11:
+                    item.quality += 2
+                if item.sell_in < 6:
+                    item.quality += 1
+                if item.sell_in < 0:
+                    item.quality = 0
+
+   
+'''
+        for item in self.items:
+            if item.category != "cheeses" and item.category != "events":
+                if item.quality > 0:
+                    if item.category != "collectors":
+                        item.quality = item.quality - 1
+            else:
+                if item.quality < 50:
+                    item.quality = item.quality + 1
+                    if item.category == "events":
+                        if item.sell_in < 11:
+                            if item.quality < 50:
+                                item.quality = item.quality + 1
+                        if item.sell_in < 6:
+                            if item.quality < 50:
+                                item.quality = item.quality + 1
+            if item.category != "collectors":
+                item.sell_in = item.sell_in - 1
+            if item.sell_in < 0:
+                if item.category != "cheeses":
+                    if item.category != "events":
+                        if item.quality > 0:
+                            if item.category != "collectors":
+                                item.quality = item.quality - 1
+                    else:
+                        item.quality = 0
+                else:
+                    if item.quality < 50:
+                        item.quality = item.quality + 1
+'''
+
+class Item:
+    def __init__(self, name, category, sell_in, quality):
+        self.name = name
+        self.category = category
+        self.sell_in = sell_in
+        self.quality = quality
+
+    def __repr__(self):
+        return "%s, %s, %s, %s" % (self.name, self.category, self.sell_in, self.quality)
---
+++ test_gilded_rose.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+import unittest
+
+from gilded_rose import Item, GildedRose
+
+class GildedRoseTest(unittest.TestCase):
+    #test -1 jour
+    def test_gr(self):
+
+        items = [
+            Item("Aged Brie", "cheeses", 2, 0),
+            Item("Sulfuras, Hand of Ragnaros", "collectors", 0, 80),
+            Item("Backstage passes to a TAFKAL80ETC concert", "events", 15, 20),
+            Item("Elixir of the Mongoose", "others", 5, 7),
+            Item("Conjured Mana Cake", "others", 3, 6)
+                ]
+
+        for i in range(30):
+            gilded_rose = GildedRose(items)
+            gilded_rose.update_quality()
+            print()
+            print("Nous sommes le jour", i, ":")
+            for x in range(len(items)):
+                print("Le produit", items[x].name, "vaut", items[x].quality,".")
+            # self.assertEqual("50", items[1].quality)
+
+if __name__ == '__main__':
+    unittest.main()
